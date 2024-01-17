31084dc05b9c1ed79e44fe5333d9966089d24685
anurag <anuragbadwahe@gmail.com>
2024-01-18 00:04:00 +0530

    Datatypes in JS

---
+++ DataTypes/primitiveDatatype.js
@@ -0,0 +1,27 @@
+// THERE ARE 7 PRIMITIVE DATATYPES IN JS
+//TRICK :- NNBBSSU
+
+let a = 7;
+let b = null;
+let c = 123422222222222222222222222222222222222n;
+let d = true;
+let e = Symbol("I am Symbol");``
+let f = undefined;
+
+console.log(a,b,c,d,e,f);
+console.log(typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(f));
+
+
+// Ques : WHY TYPE OF NULL IS OBJECT ?
+//ANS :  It is supposed to be something that doesn't exist. Unfortunately, in JavaScript, the data type of null is an object.
+
+
+//Creation of object
+
+let obj = {
+    "name" : "Anurag",
+    "job role" : "SOFTWARE DEVELOPER",
+     Degree : "B TECH"
+}
+
+console.log(obj);
