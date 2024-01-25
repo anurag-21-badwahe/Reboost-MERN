f64cdd5157786f0d52f8dac109d0ff63faeaae33
anurag <anuragbadwahe@gmail.com>
2024-01-25 22:38:18 +0530

    Promises

---
+++ 07_Promises/ex1.js
@@ -0,0 +1,51 @@
+const getUserData = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const user = { id: 1, name: 'Anurag' };
+            resolve(user);
+        }, 1000);
+    });
+};
+
+const getComments = (userId) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const comments = { id: 101, text: 'Great post!' };
+            resolve(comments);
+        }, 1000);
+    });
+};
+
+const getReplies = (commentId) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const replies = { id: 201, text: 'Thanks!' };
+            resolve(replies);
+        }, 1000);
+    });
+};
+
+const getLikes = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const likes = 200;
+            resolve(likes);
+        }, 1000);
+    });
+};
+
+
+getUserData()
+    .then((user) => getComments(user.id))
+    .then((comments) => getReplies(comments.id))
+    .then((replies) => getLikes())
+    .then(({ user, comments, replies, likes }) => {
+        console.log('User:', user);
+        console.log('Comments:', comments);
+        console.log('Replies:', replies);
+        console.log('Likes:', likes);
+        console.log(`${user.name} did ${comments.text} on this friend's post and got ${replies.text} replies and ${likes} likes`);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
---
+++ 07_Promises/promises.js
@@ -0,0 +1,59 @@
+const getUserData = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const user = { id: 1, name: 'Anurag' };
+            resolve(user);
+        }, 1000);
+    });
+};
+
+const getComments = (userId) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const comments = { id: 101, text: 'Great post!' };
+            resolve(comments);
+        }, 1000);
+    });
+};
+
+const getReplies = (commentId) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const replies = { id: 201, text: 'Thanks!' };
+            resolve(replies);
+        }, 1000);
+    });
+};
+
+const getLikes = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const likes = 200;
+            resolve(likes);
+        }, 1000);
+    });
+};
+
+getUserData()
+    .then((user) => {
+        return getComments(user.id)
+            .then((comments) => {
+                return getReplies(comments.id)
+                    .then((replies) => {
+                        return getLikes()
+                            .then((likes) => {
+                                return { user, comments, replies, likes };
+                            });
+                    });
+            });
+    })
+    .then(({ user, comments, replies, likes }) => {
+        console.log('User:', user);
+        console.log('Comments:', comments);
+        console.log('Replies:', replies);
+        console.log('Likes:', likes);
+        console.log(`${user.name} did ${comments.text} on this friend's post and got ${replies.text} replies and ${likes} likes`);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
