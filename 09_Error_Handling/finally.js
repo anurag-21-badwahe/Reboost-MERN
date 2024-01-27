d5d62ae0bd1038a23edccbb2fa067ba6cf841fda
anurag <anuragbadwahe@gmail.com>
2024-01-27 23:10:52 +0530

    Add example

--- 09_Error_Handling/finally.js
+++ 09_Error_Handling/finally.js
@@ -26,4 +26,34 @@ function main(){
 let c = main()
 
 
-// Must use finally in a function then only it will make sense
+// Must use finally in a function then only it will make sense after understanding the above code find the output of the below code
+
+// let a = prompt("Enter first number")
+
+// let b = prompt("Enter second number")
+// if (isNaN(a) || isNaN(b)) {
+//     throw SyntaxError("Sorry this is not allowed")
+// }
+
+// let sum = parseInt(a) + parseInt(b)
+
+// function main(){ 
+//     let x = 1;
+//     try {
+//         console.log("The sum is ", sum * x)
+//         return true
+        
+//     } catch (error) {
+//         console.log("Error aa gaya bhai")
+//         return false
+//     } 
+   
+//         console.log("files are being closed and db connection is being closed")
+    
+  
+// }
+
+// let c = main()
+
+
+// // Must use finally in a function then only it will make sense
