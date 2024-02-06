8e706f5e73445d657e76bfb465716cf1880ddc0d
anurag <anuragbadwahe@gmail.com>
2024-02-06 21:37:20 +0530

    Request and Query parametre in express studied

--- 12_Express/Intro_to_Express/main.js
+++ 12_Express/Intro_to_Express/main.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/',(req.res)=>{
+app.get('/',(req,res)=>{ // app.get,app.post,app.put.....(path,handler)
     res.send("Hello world to express js")
+})
+app.get('/contact',(req,res)=>{ // app.get,app.post,app.put.....(path,handler)
+    res.send("Contact page")
+})
+app.get('/userId/:slug',(req,res)=>{ // app.get,app.post,app.put.....(path,handler)
+    // res.send(`Hello ${req.params.slug}`)
+    console.log(req.params)
+    //req.params is an object
 })
 
 app.listen(port,()=>{
