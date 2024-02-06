d8e07d16785eec5b282de52b0d4acbd9f1751b19
anurag <anuragbadwahe@gmail.com>
2024-02-07 00:05:26 +0530

    Learn how to test api without postman app

---
+++ 12_Express/Request_in_express/main.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{ // app.get,app.post,app.put.....(path,handler)
+    res.send("Hello world to express js")
+})
+app.get('/contact',(req,res)=>{ // app.get,app.post,app.put.....(path,handler)
+    res.send("Contact page")
+})
+
+app.get('/userId/:slug',(req,res)=>{ // app.get,app.post,app.put.....(path,handler)
+    res.send(`Hello ${req.params.slug}`)
+    // console.log(req.query)
+    console.log(req)
+    console.log("Hello")
+
+    // request.params and request query for this url => http://localhost:3000/userId/anurag?softwareEngineer%20=%20microsoft
+    console.log(req.params)
+    console.log(req.query)
+    //req.params is an object
+})
+
+app.post('/', (req, res) => {
+    console.log("This is post request")
+    res.send('Hello World Post')
+  })
+  
+app.listen(port,()=>{
+    console.log(`Example app is listening on port ${port}`);
+})
