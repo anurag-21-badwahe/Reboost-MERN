e058737779506cdb1f74de671ea85441cfe510d5
anurag <anuragbadwahe@gmail.com>
2024-02-24 18:18:12 +0530

    Studied for each loop

---
+++ 11_Imp question/functions.js
@@ -0,0 +1,31 @@
+
+// Basic Function
+
+/*-------------------- 
+        function myFunction(g1, g2) {
+            return g1 / g2;
+        }
+        const value = myFunction(8, 2); // Calling the function
+        console.log(value);
+------------------------*/
+
+
+//Function stored in a varible
+/*
+const square = function (number) {
+    return number * number;
+};
+const x = square(4); // x gets the value 16
+console.log(x);
+*/
+
+
+//arrow function
+
+/*-------------
+   const greet = (name) => {
+    return `Hello, ${name}!`;
+};
+
+greet("Anurag")
+--------------*/
