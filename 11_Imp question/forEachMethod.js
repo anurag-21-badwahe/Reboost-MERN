c2ad0792295cb757f168d8dd1b44c82bc65ec103
anurag <anuragbadwahe@gmail.com>
2024-02-24 17:25:54 +0530

    For each method added

---
+++ 11_Imp question/forEachMethod.js
@@ -0,0 +1,26 @@
+// The forEach() method is an iterative method. 
+// It calls a provided callbackFn function once for each element in an array in ascending-index order
+
+let arr = [1,24,5,68,9];
+
+// arr.forEach(function(i){
+//     console.log(i);  
+// })
+
+
+arr.forEach((i)=>{
+    console.log(i);
+    
+})
+
+const obj = {
+    "name" : "Anurag",
+    "age" : 20
+}
+
+// Object.values(obj).forEach((i)=>{
+//     console.log(i);
+// })
+// Object.keys(obj).forEach((i)=>{
+//     console.log(i);
+// })
